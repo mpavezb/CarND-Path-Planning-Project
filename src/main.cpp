75aaa711d47636eb4059feb8438d2e111dae1bda
Matias Pavez <matias.pavez.b@gmail.com>
2020-07-26 03:48:25 +0200

    Add Map and MapReader classes

--- src/main.cpp
+++ src/main.cpp
@@ -15,83 +15,36 @@
 using nlohmann::json;
 using std::string;
 using std::vector;
+using namespace udacity;
 
 int main() {
-  uWS::Hub h;
-  udacity::MotionPlanner planner;
-
-  // Load up map values for waypoint's x,y,s and d normalized normal vectors
-  vector<double> map_waypoints_x;
-  vector<double> map_waypoints_y;
-  vector<double> map_waypoints_s;
-  vector<double> map_waypoints_dx;
-  vector<double> map_waypoints_dy;
-
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
+  Map map = MapReader::readMap(map_file_);
+  map.max_s = max_s;
+  MotionPlanner planner{map};
 
-  std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
-
-  string line;
-  while (getline(in_map_, line)) {
-    std::istringstream iss(line);
-    double x;
-    double y;
-    float s;
-    float d_x;
-    float d_y;
-    iss >> x;
-    iss >> y;
-    iss >> s;
-    iss >> d_x;
-    iss >> d_y;
-    map_waypoints_x.push_back(x);
-    map_waypoints_y.push_back(y);
-    map_waypoints_s.push_back(s);
-    map_waypoints_dx.push_back(d_x);
-    map_waypoints_dy.push_back(d_y);
-  }
-
-  h.onMessage([&map_waypoints_x, &map_waypoints_y, &map_waypoints_s,
-               &map_waypoints_dx, &map_waypoints_dy,
-               &planner](uWS::WebSocket<uWS::SERVER> ws, char *data,
+  uWS::Hub h;
+  h.onMessage([&planner](uWS::WebSocket<uWS::SERVER> ws, char *data,
                          size_t length, uWS::OpCode opCode) {
-    // "42" at the start of the message means there's a websocket message event.
-    // The 4 signifies a websocket message
-    // The 2 signifies a websocket event
+    // "42" at the start of the message means there's a websocket message
+    // event. The 4 signifies a websocket message The 2 signifies a
+    // websocket event
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
       auto s = hasData(data);
-
       if (s != "") {
         auto j = json::parse(s);
-
         string event = j[0].get<string>();
-
         if (event == "telemetry") {
-          const json json_packet = j[1];
-          const udacity::TelemetryParser parser;
-          const auto telemetry_packet = parser.fromJson(json_packet);
-
-          // Previous path data given to the Planner
-          auto previous_path_x = json_packet["previous_path_x"];
-          auto previous_path_y = json_packet["previous_path_y"];
-          // Previous path's end s and d values
-          double end_path_s = json_packet["end_path_s"];
-          double end_path_d = json_packet["end_path_d"];
-
-          // Sensor Fusion Data, a list of all other cars on the same side
-          //   of the road.
-          auto sensor_fusion = json_packet["sensor_fusion"];
-
-          // Motion Planning
+          const json packet = j[1];
+          const auto telemetry_packet = TelemetryParser::fromJson(packet);
           planner.generate_trajectory(telemetry_packet);
 
           json msgJson;
           msgJson["next_x"] = planner.getNextXVals();
           msgJson["next_y"] = planner.getNextYVals();
-
           auto msg = "42[\"control\"," + msgJson.dump() + "]";
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
--- src/planner.h
+++ src/planner.h
@@ -1,6 +1,7 @@
 #ifndef PLANNER_H
 #define PLANNER_H
 
+#include <fstream>
 #include <vector>
 
 #include "helpers.h"
@@ -8,32 +9,90 @@
 
 namespace udacity {
 
+struct Map {
+  double max_s{0.0};
+  std::string filename;
+  std::vector<double> waypoints_x;
+  std::vector<double> waypoints_y;
+  std::vector<double> waypoints_s;
+  std::vector<double> waypoints_dx;
+  std::vector<double> waypoints_dy;
+};
+
+class MapReader {
+ public:
+  static Map readMap(const std::string& filename) {
+    Map map{};
+    std::ifstream in_map_(filename.c_str(), std::ifstream::in);
+    string line;
+    while (getline(in_map_, line)) {
+      std::istringstream iss(line);
+      double x;
+      double y;
+      float s;
+      float d_x;
+      float d_y;
+      iss >> x;
+      iss >> y;
+      iss >> s;
+      iss >> d_x;
+      iss >> d_y;
+      map.waypoints_x.push_back(x);
+      map.waypoints_y.push_back(y);
+      map.waypoints_s.push_back(s);
+      map.waypoints_dx.push_back(d_x);
+      map.waypoints_dy.push_back(d_y);
+    }
+    map.filename = filename;
+    return map;
+  }
+};
+
 struct TelemetryPacket {
+  // Main car's localization Data
   double car_x;
   double car_y;
   double car_s;
   double car_d;
   double car_yaw;
   double car_speed;
+
+  // A list of all other cars on the same side the road.
+  // auto sensor_fusion;
+
+  // Previous path data given to the Planner
+  // auto previous_path_x;
+  // auto previous_path_y;
+
+  // Previous path's end s and d values
+  double end_path_s;
+  double end_path_d;
 };
 
 class TelemetryParser {
  public:
-  TelemetryPacket fromJson(const nlohmann::json& data) const {
+  static TelemetryPacket fromJson(const nlohmann::json& data) {
     TelemetryPacket packet;
-    // Main car's localization Data
     packet.car_x = data["x"];
     packet.car_y = data["y"];
     packet.car_s = data["s"];
     packet.car_d = data["d"];
     packet.car_yaw = data["yaw"];
     packet.car_speed = data["speed"];
+    // previous_path_x = data["previous_path_x"];
+    // previous_path_y = data["previous_path_y"];
+    packet.end_path_s = data["end_path_s"];
+    packet.end_path_d = data["end_path_d"];
+    // auto sensor_fusion = data["sensor_fusion"];
+
     return packet;
   }
 };
 
 class MotionPlanner {
  public:
+  MotionPlanner(const Map& map) : map_(map) {}
+
   void generate_trajectory(const TelemetryPacket& telemetry) {
     /**
      * TODO: define a path made up of (x,y) points that the car will visit
@@ -56,7 +115,9 @@ class MotionPlanner {
  private:
   std::vector<double> next_x_vals;
   std::vector<double> next_y_vals;
+  Map map_;
 };
+
 }  // namespace udacity
 
 #endif  // PLANNER_H
